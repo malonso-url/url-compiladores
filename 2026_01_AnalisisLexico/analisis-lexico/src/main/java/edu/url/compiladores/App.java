ac80f75923fa02a87c56e795b6821402f8754a9d
Moises Alonso <digiprodsgt@gmail.com>
2026-01-21 20:45:20 -0600

    Carga inicial del proyecto

---
+++ 2026_01_AnalisisLexico/analisis-lexico/src/main/java/edu/url/compiladores/App.java
@@ -0,0 +1,13 @@
+package edu.url.compiladores;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
